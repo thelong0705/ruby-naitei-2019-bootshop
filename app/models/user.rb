67debe9e1811192d60b151bc3ebde071e49e5b5f
The Long <thelong0705@gmail.com>
2019-08-15 13:44:19 +0700

    add test

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,13 @@ class User < ApplicationRecord
 
   has_many :credit_cards, dependent: :destroy
   after_commit :assign_customer_id, on: :create
-
-  def assign_customer_id
-    customer = Stripe::Customer.create email: email
-    self.customer_id = customer.id
-  end
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
+  
+  # def assign_customer_id
+  #   customer = Stripe::Customer.create email: email
+  #   self.customer_id = customer.id
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema.define(version: 20190813071112) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20190813081726) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
@@ -7,6 +19,12 @@ ActiveRecord::Schema.define(version: 20190813071112) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "product_id"], name: "index_carts_on_user_id_and_product_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "credit_cards", force: :cascade do |t|
@@ -23,8 +41,10 @@ ActiveRecord::Schema.define(version: 20190813071112) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
-  
+
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
@@ -35,7 +55,7 @@ ActiveRecord::Schema.define(version: 20190813071112) do
     t.index ["name"], name: "index_roles_on_name"
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
@@ -54,7 +74,7 @@ ActiveRecord::Schema.define(version: 20190813071112) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  
+
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "role_id"
@@ -62,20 +82,5 @@ ActiveRecord::Schema.define(version: 20190813071112) do
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
-  
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-  
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.float "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "image"
-    t.integer "category_id"
-    t.index ["category_id"], name: "index_products_on_category_id"
-  end
+
 end
