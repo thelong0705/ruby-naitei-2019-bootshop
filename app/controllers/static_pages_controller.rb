79d82c368b6208e48204722f38145075757b4fae
tungts1101 <tungts1101@gmail.com>
2019-08-21 15:02:26 +0700

    fix form in ransack

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def index
-    @q = Product.ransack params[:q]
+    @q = Product.ransack name_cont: params[:q]
     @products = @q.result.page(params[:page]).per(7)
   end
 end
